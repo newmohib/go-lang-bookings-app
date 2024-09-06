6eaf6663656c9bff35297a35758ef1abe9d1a1ca
newmohib <@@mohib100@@>
2024-09-06 15:42:04 +0600

    added ajax call for send form data to as a post

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -39,7 +39,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/search-availability", handlers.Repo.SearchAvailability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
-	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
